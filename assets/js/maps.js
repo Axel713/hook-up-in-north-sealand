4bdd34e78411775d14bb556ed285075b674266b6
Axel Caille <axel@quasyl.com>
2020-11-28 22:59:47 +0100

    This commits adds content to the Fishing in North Seland section, especially what do the relevant interactive sections do for the user.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -27,3 +27,21 @@ function initMap() {
 			"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
 	});
 }
+
+/* Locations to introduce
+
+Sea
+{ lat: 56.035190, lng: 12.619449 }, ok
+{ lat: 55.992499, lng: 12.561722 }, ok
+{ lat: 56.093426, lng: 12.467988 }, ok
+{ lat: 56.055800, lng: 12.056413 }, ok
+{ lat: 55.965391, lng: 11.842141 } ok
+
+
+Forbidden:
+{ lat: 55.929741, lng: 12.522639 }, ok
+{ lat: 56.099830, lng: 12.402078 }, ok
+{ lat: 56.103266, lng: 12.380269 }, ok
+{ lat: 56.126424, lng: 12.308342 }, ok
+{ lat: 56.106938, lng: 12.177238 }, ok 
+{ lat: 56.099063, lng: 12.160142 } ok*/
