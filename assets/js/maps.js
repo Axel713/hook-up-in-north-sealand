f706e596feed2c421c803ca4819a065afd54e768
Axel Caille <axel@quasyl.com>
2020-11-30 01:15:20 +0100

    This commits add commented out geoloactions for lake, sea and forbidden area markers as reference for future purpose.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -27,3 +27,28 @@ function initMap() {
 			"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
 	});
 }
+
+/*const locations = [
+    { lat: 56.00389, lng: 12.37755 },
+    { lat: 55.9759, lng: 12.11008 }
+];
+Lake
+
+const locations = [
+    { lat: 56.03519, lng: 12.619449 },
+    { lat: 55.992499, lng: 12.561722 },
+    { lat: 56.093426, lng: 12.467988 },
+    { lat: 56.0558, lng: 12.056413 },
+    { lat: 55.965391, lng: 11.842141 }
+];
+Sea
+
+const locations = [
+    { lat: 55.929741, lng: 12.522639 },
+    { lat: 56.09983, lng: 12.402078 },
+    { lat: 56.103266, lng: 12.380269 },
+    { lat: 56.126424, lng: 12.308342 },
+    { lat: 56.106938, lng: 12.177238 },
+    { lat: 56.099063, lng: 12.160142 }
+];
+Forbidden*/
