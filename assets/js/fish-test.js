90c13c26bae4b71658a31f073a61aaf23998ecac
Axel Caille <axel@quasyl.com>
2020-12-06 16:25:28 +0100

    This commit adds the begining of the function fishTest to fish-test.js, including fishData object.

---
+++ assets/js/fish-test.js
@@ -0,0 +1,38 @@
+function fishTest() {
+    // Declare fish data object
+    const fishData = {
+      fishCat: [
+        {
+          name: "lavaret",
+          seasonStart: "11/01/2020",
+          seasonEnd: "01/31/2021",
+          size: 36
+        },
+        {
+          name: "mackerel",
+          seasonStart: false,
+          seasonEnd: false,
+          size: 20
+        },
+        {
+          name: "perch",
+          seasonStart: false,
+          seasonEnd: false,
+          size: 20
+        },
+        {
+          name: "pike",
+          seasonStart: "04/01/2021",
+          seasonEnd: "04/30/2021",
+          size: 60
+        },
+        {
+          name: "plaice",
+          seasonStart: "01/15/2021",
+          seasonEnd: "01/30/2021",
+          size: 27
+        }
+      ]
+    };
+
+    
