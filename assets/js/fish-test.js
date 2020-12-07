d1029728bec0721a84559aa78e09a73d491d5d01
Axel Caille <axel@quasyl.com>
2020-12-07 22:48:58 +0100

    Removed commented out code in fish-test js file.

--- assets/js/fish-test.js
+++ assets/js/fish-test.js
@@ -1,3 +1,9 @@
+/**
+ * The fishTest() function is used by the Can I take my Catch Home section of the project.
+ * It allows the user to verify if today's catch date is ok according to various species of fish.
+ * It also allows to verify if the length of the caught fish is enough for the user to keep it or
+ * if he must release it.
+ */
 function fishTest() {
 	// Declare fish data object
 	const fishData = {
@@ -38,34 +44,24 @@ function fishTest() {
 	// Obtain value from "fishList" dropdown list
 	const selectedFish = document.getElementById("fishList");
 	const selFishValue = selectedFish.options[selectedFish.selectedIndex].value;
-	// Console log selected value
-	//console.log("Selected value drom dropdown list: ", selFishValue);
 
 	// Access and store fishCat array of objects content in a variable
 	const fishCatVar = fishData.fishCat;
-	// console.log(fishCatVar);
 
 	// For loop based on fishCat array
 	for (let i = 0; i < fishCatVar.length; i++) {
-		//console.log(fishCatVar[i]);
-		//console.log(fishCatVar[i].name);
-		//console.log(fishCatVar[i]["seasonStart"]);
-
-		// If conditional statement to match selected value from dropdownlist to object data in fishData object.
+		// Match selected value from dropdownlist to object data in fishData object.
 		if (selFishValue === fishCatVar[i].name) {
 			// Store matching fish in variable
 			const filtFish = fishCatVar[i];
-			//console.log(filtFish);
-			//console.log(filtFish.seasonStart, filtFish.seasonEnd);
 
 			// ----------------------------------------------------------------------------- DATE TEST -----------------------------------------------------------------------------
-			// If conditional statement to process fish without restriced period.
+			// Process fish without restriced period.
 			if (filtFish.seasonStart == false && filtFish.seasonEnd == false) {
-				//console.log("Passed false test");
 				document.getElementById("verdict-date").innerHTML =
 					"No restricted period. Test your fish size below!";
 			} else {
-				// else statement to process fish with restricted period.
+				// To process fish with restricted period.
 				// Today's date.
 				const myToday = new Date();
 				// Store myToday in a variable and converts it to the number of milliseconds since the Unix Epoch.
@@ -74,24 +70,20 @@ function fishTest() {
 				// Store seasonStart and seasonEnd dates in discreet variables and convert them to the number of milliseconds since the Unix Epoch.
 				const startSeas = Date.parse(filtFish.seasonStart);
 				const endSeas = Date.parse(filtFish.seasonEnd);
-				//console.log(startSeas, endSeas);
 
-				// If-else statement to process and give verdict from restriced period dates.
+				// Process and give verdict from restriced period dates.
 				if (msMyToday < startSeas || msMyToday > endSeas) {
 					document.getElementById("verdict-date").innerHTML =
 						"Within allowed period. Test your fish size below!";
-					//console.log("OK");
 				} else {
 					document.getElementById("verdict-date").innerHTML =
 						"Outside allowed period. Release your fish.";
-					//console.log("NOT OK");
 				}
 			}
 
 			// ----------------------------------------------------------------------------- SIZE TEST -----------------------------------------------------------------------------
 			// Access and store filtered fish control size in a variable.
 			const setFishSize = filtFish.size;
-			//console.log(setFishSize);
 
 			// Access and store button in a variable.
 			const sizeTestButton = document.getElementById("sizeTestBtn");
@@ -101,7 +93,7 @@ function fishTest() {
 				// Access and record value entered in input field in a variable
 				const fishSize = document.getElementById("fishSize").value;
 
-				// If-else statement to produce verdict
+				// Output size test verdict verdict
 				if (fishSize >= setFishSize) {
 					document.getElementById("verdict-size").innerHTML =
 						"You can keep your fish!";
